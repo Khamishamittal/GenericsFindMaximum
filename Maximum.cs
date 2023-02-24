3b254b6b4b5b08e395f226b89455f5fbed75af66
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-24 14:51:16 +0530

    Given 3 integers find Maximum of them

---
+++ Maximum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Maximum {
+        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                    firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                    firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+              {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue )> 0 ||
+                    secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                    secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+              {
+                return secondvalue;
+
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue )> 0 ||
+                    thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                    thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+              {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+     }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to GenericsFindMaximum Problem");
+             int num1 = 50, num2 = 30, num3 = 56;
+            int value = Maximum.MaximumIntegerNumber(num1, num2, num3);
+            Console.WriteLine(value + " is greater than other two others");
 
         }
-        }
+    }
 
     }
 
