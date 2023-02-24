d3fe95802d315db8cd25913451cdb44ce6883441
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-24 14:31:40 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+
+
+    class Program
+    {
+        
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GenericsFindMaximum Problem");
+
+        }
+        }
+
+    }
+
