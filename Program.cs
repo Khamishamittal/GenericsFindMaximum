9c756da7a04d10b47b0763a0b460e4b94a29ef60
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-24 21:55:10 +0530

    Create a Generic Program Using Method

--- Program.cs
+++ Program.cs
@@ -11,8 +11,20 @@
         {
             Console.WriteLine("Welcome to GenericsFindMaximum Problem");
 
+            int max1 = UsingMethod.FindMaxValue<int>(80, 45, 60);
+            Console.WriteLine($"{max1} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 2: ");
+            float max2 = UsingMethod.FindMaxValue<float>(4.5f, 7.5f, 3.3f);
+            Console.WriteLine($"{max2} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 3: ");
+            string max3 = UsingMethod.FindMaxValue<string>("Peach", "Apple", "Orange");
+            Console.WriteLine($"{max3} is the greatest among three number");
         }
-        }
+    }
 
     }
 
