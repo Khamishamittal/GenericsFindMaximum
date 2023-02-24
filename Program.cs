95bbe16e13fb6d975d15dd977c9f4d7158bb881a
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-24 18:34:17 +0530

    Create a Program to find Max float Number

--- Program.cs
+++ Program.cs
@@ -10,12 +10,29 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to GenericsFindMaximum Problem");
-             int num1 = 50, num2 = 30, num3 = 56;
-            int value = Maximum.MaximumIntegerNumber(num1, num2, num3);
-            Console.WriteLine(value + " is greater than other two others");
+
+            Console.WriteLine("Executing three test cases to find the maximum Float number among three numbers");
+
+            Console.WriteLine("\nTest case 1: ");
+            float max1 = MaxFloat.FindMaxFloat(6.5f, 4.5f, 3.2f);
+            Console.WriteLine($"{max1} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 2: ");
+            float max2 = MaxFloat.FindMaxFloat(4.1f, 7.2f, 3.3f);
+            Console.WriteLine($"{max2} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 3: ");
+            float max3 = MaxFloat.FindMaxFloat(2.2f, 4.5f, 5.6f);
+            Console.WriteLine($"{max3} is the greatest among three number");
+
 
         }
     }
 
-    }
+}
+    
+
+    
 
